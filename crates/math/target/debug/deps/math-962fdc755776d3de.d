670be06536a2a6d315ce66a7816268c0f2e90571
Teeco123 <dastardly7761@proton.me>
2026-05-22 01:04:59 +0200

    fix: deleted targets

--- crates/math/target/debug/deps/math-962fdc755776d3de.d
+++
@@ -1,6 +0,0 @@
-/home/kacper/projects/aerin-rs/crates/math/target/debug/deps/math-962fdc755776d3de.d: src/lib.rs src/vec2.rs
-
-/home/kacper/projects/aerin-rs/crates/math/target/debug/deps/libmath-962fdc755776d3de.rmeta: src/lib.rs src/vec2.rs
-
-src/lib.rs:
-src/vec2.rs:
--- crates/math/target/debug/deps/math-fa00a7187114f577.d
+++
@@ -1,6 +0,0 @@
-/home/kacper/projects/aerin-rs/crates/math/target/debug/deps/math-fa00a7187114f577.d: src/lib.rs src/vec2.rs
-
-/home/kacper/projects/aerin-rs/crates/math/target/debug/deps/libmath-fa00a7187114f577.rmeta: src/lib.rs src/vec2.rs
-
-src/lib.rs:
-src/vec2.rs:
