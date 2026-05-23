b4756e4094ca8e32fdc67c7c3bc678f4538e62ad
Teeco123 <dastardly7761@proton.me>
2026-05-24 00:10:16 +0200

    feat: opengl context

--- crates/window/src/lib.rs
+++ crates/window/src/lib.rs
@@ -1,6 +1,16 @@
+use std::num::NonZeroU32;
+
+use glutin::{
+    config::ConfigTemplateBuilder,
+    context::{ContextApi, ContextAttributesBuilder, Version},
+    display::Display,
+    prelude::{GlDisplay, NotCurrentGlContext},
+    surface::{SurfaceAttributesBuilder, WindowSurface},
+};
 use winit::{
     application::ApplicationHandler,
     event_loop::{ControlFlow, EventLoop},
+    raw_window_handle::{HasDisplayHandle, HasWindowHandle},
     window::Window,
 };
 
@@ -18,7 +28,48 @@ impl ApplicationHandler for WinitApp {
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
         println!("winit resumed");
         let winit_window_attributes = Window::default_attributes();
-        self.window = Some(event_loop.create_window(winit_window_attributes).unwrap());
+        let window = event_loop.create_window(winit_window_attributes).unwrap();
+
+        let raw_window_handle = window.window_handle().unwrap();
+        let raw_display_handle = window.display_handle().unwrap();
+
+        let preference = glutin::display::DisplayApiPreference::Egl;
+
+        let gl_display = unsafe { Display::new(raw_display_handle.as_raw(), preference).unwrap() };
+
+        let template = ConfigTemplateBuilder::new();
+
+        let gl_config = unsafe {
+            gl_display
+                .find_configs(template.build())
+                .unwrap()
+                .next()
+                .unwrap()
+        };
+
+        let context_attributes = ContextAttributesBuilder::new()
+            .with_context_api(ContextApi::OpenGl(Some(Version::new(3, 3))))
+            .build(Some(raw_window_handle.as_raw()));
+
+        let not_current = unsafe {
+            gl_display
+                .create_context(&gl_config, &context_attributes)
+                .unwrap()
+        };
+
+        let attrs = SurfaceAttributesBuilder::<WindowSurface>::new().build(
+            raw_window_handle.as_raw(),
+            NonZeroU32::new(800).unwrap(),
+            NonZeroU32::new(800).unwrap(),
+        );
+
+        let surface = unsafe {
+            gl_display
+                .create_window_surface(&gl_config, &attrs)
+                .unwrap()
+        };
+
+        let context = not_current.make_current(&surface).unwrap();
     }
     fn window_event(
         &mut self,
