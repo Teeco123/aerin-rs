40e1b15d0d417c71afc045c0635dfee0f0d3e57f
Teeco123 <dastardly7761@proton.me>
2026-07-29 01:01:51 +0200

    feat: more mat4 fn

--- src/math/mat4.rs
+++ src/math/mat4.rs
@@ -24,9 +24,53 @@ impl Mat4 {
         let (sin, cos) = angle.sin_cos();
         Self {
             x_col: Vec4::new(1.0, 0.0, 0.0, 0.0),
-            y_col: Vec4::new(0.0, cos, -sin, 0.0),
-            z_col: Vec4::new(0.0, sin, cos, 0.0),
-            w_col: Vec4::new(0.0, 0.0, 0.0, 0.0),
+            y_col: Vec4::new(0.0, cos, sin, 0.0),
+            z_col: Vec4::new(0.0, -sin, cos, 0.0),
+            w_col: Vec4::new(0.0, 0.0, 0.0, 1.0),
         }
+    }
+
+    #[inline]
+    pub fn rotate_y(angle: f32) -> Self {
+        let (sin, cos) = angle.sin_cos();
+        Self {
+            x_col: Vec4::new(cos, 0.0, -sin, 0.0),
+            y_col: Vec4::new(0.0, 1.0, 0.0, 0.0),
+            z_col: Vec4::new(sin, 0.0, cos, 0.0),
+            w_col: Vec4::new(0.0, 0.0, 0.0, 1.0),
+        }
+    }
+
+    #[inline]
+    pub fn rotate_z(angle: f32) -> Self {
+        let (sin, cos) = angle.sin_cos();
+        Self {
+            x_col: Vec4::new(cos, sin, 0.0, 0.0),
+            y_col: Vec4::new(-sin, cos, 0.0, 0.0),
+            z_col: Vec4::new(0.0, 0.0, 1.0, 0.0),
+            w_col: Vec4::new(0.0, 0.0, 0.0, 1.0),
+        }
+    }
+
+    #[inline]
+    pub fn to_array(&self) -> [f32; 16] {
+        [
+            self.x_col.x,
+            self.x_col.y,
+            self.x_col.z,
+            self.x_col.w,
+            self.y_col.x,
+            self.y_col.y,
+            self.y_col.z,
+            self.y_col.w,
+            self.z_col.x,
+            self.z_col.y,
+            self.z_col.z,
+            self.z_col.w,
+            self.w_col.x,
+            self.w_col.y,
+            self.w_col.z,
+            self.w_col.w,
+        ]
     }
 }
